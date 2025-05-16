0a58918997e135d94f21cc3683e5650cde2703fb
LuanaM1 <malaquias.luana@escola.pr.gov.br>
2025-05-16 20:12:08 -0300

    Add files via upload

---
+++ agrinho_2025_05_16_23_10_37/sketch.js
@@ -0,0 +1,59 @@
+let jogador;
+let teclaPressionada = {}; //objeto vazio
+let jogadorSprite;
+let world;
+
+function preload() {
+  jogadorSprite = loadImage("Sprites/fazendeira.png");
+  world = loadImage("Sprites/fundo.png");
+}
+
+function setup() {
+  createCanvas(600, 400);
+  jogador = new Jogador(width / 2, height / 2);
+}
+
+function draw() {
+  background(220);
+  image(world, 0, 0, 600, 400);
+
+  jogador.update();
+  jogador.draw();
+}
+function keyPressed() {
+  teclaPressionada[key] = true;
+}
+function keyReleased() {
+  delete teclaPressionada[key];
+}
+
+class Jogador {
+  constructor(x, y) {
+    this.x = x;
+
+    this.y = y;
+    this.speed = 4; //define velocidade
+  }
+  update() {
+    console.log(teclaPressionada)
+    let mvmt = createVector(0, 0); // Cria um objeto vector
+    if (teclaPressionada.a) {
+      mvmt.x -= 1;
+    }
+    if (teclaPressionada.d) {
+      mvmt.x += 1;
+    }
+    if (teclaPressionada.w) {
+      mvmt.y -= 1;
+    }
+
+      mvmt.setMag(this.speed);
+    
+
+    this.x += mvmt.x;
+    this.y += mvmt.y;
+  }
+  draw() {
+    image(jogadorSprite, this.x, this.y, 100, 100);
+  }
+}
