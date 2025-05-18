78167c4ec8c1ca67baf25bb635ca49952c3d04ec
LuanaM1 <malaquias.luana@escola.pr.gov.br>
2025-05-18 17:13:31 -0300

    versão 4
    adicionado música, caixa de dialogo, e outros

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ let jogadorSprite = [];
 let world = [];
 let currentLevel = 0;
 let bgMusic;
+let dialoguebox = [];
 
 function preload() {
   jogadorSprite[0] = loadImage("Sprites/fazendeira.png");
@@ -15,6 +16,7 @@ function preload() {
 
   world[0] = loadImage("Sprites/fundo.png");
   world[1] = loadImage("Sprites/fundo1.png");
+  dialoguebox[0] = loadImage("Sprites/dialogueBoxes/1.png");
   bgMusic = loadSound("Música/beBorn.mp3");
 }
 
@@ -26,15 +28,18 @@ function setup() {
 }
 
 function draw() {
+  console.log(mouseX, mouseY);
   background(220);
-  noSmooth();
+  noSmooth(); //´para não suavizar a imagem de pixel art pois perde a qualidade
   image(world[currentLevel], 0, 0, 600, 400);
+
   jogador.update();
   jogador.draw();
+  image(dialoguebox[0], 50, 250);
 }
 
 function keyPressed() {
-  teclaPressionada[key.toLowerCase()] = true;
+  teclaPressionada[key.toLowerCase()] = true; // para evitar erros quando uma letra maiuscula é apertada
 }
 
 function keyReleased() {
@@ -53,14 +58,14 @@ class Jogador {
     this.isFlipped = false;
     this.currentFrame = 0;
     this.animationCounter = 0;
-    this.animationSpeed = 8;
+    this.animationSpeed = 8; //quanto maior o numero mais lento a troca de frame!
     this.isMoving = false;
     this.isJumping = false;
     this.jumpForce = -12;
     this.gravity = 0.6;
     this.jumpVelocity = 0;
     this.groundY = y;
-    this.canJump = true;
+    this.canJump = true; //impede que o jogador faça pulos infinitamente.
     this.IsIdle = true;
     this.idleFrame = 0;
     this.animationIdleSpeed = 20;
@@ -69,6 +74,7 @@ class Jogador {
   update() {
     let mvmt = createVector(0, 0);
 
+    // movimento e a inversão do JogadorSprite
     if (teclaPressionada.a) {
       mvmt.x -= 1;
       this.isFlipped = true;
@@ -80,7 +86,10 @@ class Jogador {
     if (teclaPressionada.w && this.canJump && !this.isJumping) {
       this.jumpVelocity = this.jumpForce;
       this.isJumping = true;
-      this.canJump = false;
+      this.canJump = false; //impede que o jogador faça outros pulos no ar.
+    }
+    if (teclaPressionada.e) {
+      dialogue();
     }
 
     this.jumpVelocity += this.gravity;
@@ -97,11 +106,9 @@ class Jogador {
     this.y += mvmt.y;
     console.log(this.x);
 
-    // In your Jogador class update() method, modify the boundary checks:
     if (this.x >= 605) {
-      // Right edge
+      // fim do canvas direito
       if (currentLevel < world.length - 1) {
-        //checa pra ver se o level e menor que o tamnanho da lista world - 1
         currentLevel++;
         this.x = 0;
         this.y = height / 2;
@@ -117,11 +124,11 @@ class Jogador {
         this.x = width - 30;
         this.y = height / 2;
       } else {
-        this.x = -30 + this.speed;
+        this.x = -30 + this.speed; //impedir que o jogador ultrapasse o fim do canvas definido como -30
       }
     }
 
-    this.isMoving = mvmt.x !== 0;
+    this.isMoving = mvmt.x !== 0; //checa se o movimento diferente de zero para ativar animação
     if (this.isMoving) {
       this.animationCounter++;
       if (this.animationCounter >= this.animationSpeed) {
@@ -132,7 +139,7 @@ class Jogador {
       this.currentFrame = 0;
     }
 
-    this.isIdle = mvmt.x === 0 && !this.isJumping;
+    this.isIdle = mvmt.x === 0 && !this.isJumping; //checa se o movimento é igual a zero e o jogador não esta pulando, evitando assim a animação Idle durante movimento ou pulo.
     if (this.isIdle) {
       this.animationCounter++;
       if (this.animationCounter >= this.animationIdleSpeed) {
@@ -145,7 +152,8 @@ class Jogador {
   draw() {
     push();
     translate(this.x, this.y);
-    if (this.isFlipped) scale(-1, 1);
+
+    if (this.isFlipped) scale(-1, 1); //inverte se necessario
     noSmooth();
 
     if (this.isJumping) {
@@ -153,8 +161,15 @@ class Jogador {
     } else if (this.isMoving) {
       image(jogadorSprite[this.currentFrame + 2], -45, 0, 90, 100);
     } else {
-      image(jogadorSprite[this.idleFrame], -45, 0, 90, 100);
+      image(jogadorSprite[this.idleFrame], -45, 0, 90, 100); //Animação para quando o jogador não esta se movendo.
     }
     pop();
+  }
+}
+function dialogue() {
+  if (currentLevel === 1) {
+    if (jogador.x >= 40 && jogador.x <= 200) {
+      console.log("hi");
+    }
   }
 }
